7c5bfd186879a36684750e13b167f05850bf89d5
RubyKumari <ruby226.driems@gmail.com>
2024-02-23 02:19:38 +0530

    dynamic header menu has been added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  menuType: string = 'default'
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((route: any) => {
+      console.warn(route.url, "Route")
+      if (localStorage.getItem('seller') && route.url.includes("seller-home")) {
+        this.menuType = 'seller'
+      }
+      else{
+        this.menuType = 'default'
+      }
+    }
+    )
+  }
 }
